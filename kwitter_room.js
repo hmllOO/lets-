95eb484ba67061863f07d1d0a6bbbe238f48d27a
JimJam_Warrior <95523775+hmllOO@users.noreply.github.com>
2023-01-13 20:45:23 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAoB8DAnPvKh08eEU8ognQzhWsm1-bib3o",
+    authDomain: "lets-chat-bed67.firebaseapp.com",
+    databaseURL: "https://lets-chat-bed67-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-bed67",
+    storageBucket: "lets-chat-bed67.appspot.com",
+    messagingSenderId: "233266194452",
+    appId: "1:233266194452:web:cb0fcb37136691082f1f01"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
